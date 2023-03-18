2037052027f2c78c09ff4d5d3819b7fbb4a9f8b6
Prince <princenzu51@gmail.com>
2023-03-18 22:35:11 +0100

    add password strenght checker

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const lower_case = "abcdefghijklmnopqrstuvwxyz";
 const upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbol = "!@#$%^&*/?><";
+const err = document.getElementById("err")
+let showPassword = document.getElementById("Show")
 
 let use_for = lower_case + upper_case + numbers + symbol;
 
 
-let passwordGenerator = document.getElementById("password")
+let passwordGenerator = document.getElementById("password");
+
 
 function generator(){
     let password = ""
@@ -24,12 +27,27 @@ function generator(){
     
 }
 
+function checkPassword(){
+    if(passwordGenerator.value.length <= 5){
+        err.innerText = "password too weak"
+        err.style.color = "red";
+    }else if(passwordGenerator.value.length <= 15){
+        err.innerHTML = "password weak"
+        err.style.color = "red";
+    }else{err.innerHTML = "password strong"
+        err.style.color = "green"}
+}
+
 
 
 function clearInput(){
     if(passwordGenerator.value != ""){
         passwordGenerator.value = "";
     }
+    if(err.value != ""){
+        err.value = "";
+    }
+    
 }
 
 function copyPassword(){
@@ -37,3 +55,12 @@ function copyPassword(){
     passwordGenerator.setSelectionRange(0, 9999999);
     document.execCommand("copy")
 }
+
+function show(){
+    if(passwordGenerator.type == "password"){
+        passwordGenerator.type = "text"
+        showPassword.innerText = "Hide";
+    }else{passwordGenerator.type = "password"
+        showPassword.innerText = "Show"}
+}
+
